e4db7411d49e1ff6e5271b813b54fb313baba8ad
Usman Durrani <usmandgreat@gmail.com>
2015-11-18 15:33:58 +0500

    Dialog Scaling Fixed

--- app/src/main/java/com/fournodes/ud/pranky/SoundSelect.java
+++ app/src/main/java/com/fournodes/ud/pranky/SoundSelect.java
@@ -45,6 +45,17 @@ public class SoundSelect extends Activity implements FileChooser.FileSelectedLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soundselect);
 
+        decorView = getWindow().getDecorView();
+
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+
         prankyDB = new PrankyDB(SoundSelect.this);
         final EditText txtselsound = (EditText) findViewById(R.id.txtSelSound);
         ImageView btnselsound = (ImageView) findViewById(R.id.btnMusicToggle);
